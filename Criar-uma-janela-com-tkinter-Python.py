4ceaa5eab1c0ef5ea2decbb98f5b3de5897518e3
Marcio Jose Alves <marciojosealves2@gmail.com>
2023-02-05 13:27:40 -0300

    Responsividade da aplicação

--- Criar-uma-janela-com-tkinter-Python.py
+++ Criar-uma-janela-com-tkinter-Python.py
@@ -1,10 +1,24 @@
 from tkinter import *
 from tkinter import ttk
 
-minha_aplicacao= Tk()
-minha_aplicacao.title("Minha Aplicação")#Título da janela
+minha_aplicacao = Tk()
+
+ # Título da janela
+minha_aplicacao.wm_title("Minha Aplicação") 
+
+ # Logo ícone.
 minha_aplicacao.iconbitmap('logo-mjamf.ico')
 
-texte_inicial = ttk.Label(text = "Olá Márcio José!!!").place(x=1, y=25) # Pode-se usar o grid(row=1, column = 25) para caso específicos.
+# Pode-se usar o grid(row=1, column = 25) para caso específicos.
+texto_inicial = ttk.Label(text="Olá Márcio José!!!").place(x=1, y=25)
+
+# Responsividade
+minha_aplicacao.wm_geometry('700x400')
+minha_aplicacao.wm_resizable(width=False, height=False)
+#minha_aplicacao.wm_maxsize(width=900,height=600)
+#minha_aplicacao.wm_minsize(width=400,height=200)
+
+
+
 
 minha_aplicacao.mainloop()
