e783bbb8f38cdb6f56c2e24a6ff3238c46189442
Marcio Jose Alves <marciojosealves2@gmail.com>
2023-02-05 12:57:15 -0300

    Acréscimo de título, logo e renomeação de ícone.

--- Criar-uma-janela-com-tkinter-Python.py
+++ Criar-uma-janela-com-tkinter-Python.py
@@ -2,7 +2,9 @@ from tkinter import *
 from tkinter import ttk
 
 minha_aplicacao= Tk()
+minha_aplicacao.title("Minha Aplicação")#Título da janela
+minha_aplicacao.iconbitmap('logo-mjamf.ico')
 
-texte_inicial = ttk.Label(text = "Olá Márcio José!!!").place(x=1, y=25)
+texte_inicial = ttk.Label(text = "Olá Márcio José!!!").place(x=1, y=25) # Pode-se usar o grid(row=1, column = 25) para caso específicos.
 
 minha_aplicacao.mainloop()
