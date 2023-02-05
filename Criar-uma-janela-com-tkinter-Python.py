4f46d9acf1dd5126655365dde5aba216d101be03
Marcio Jose Alves <marciojosealves2@gmail.com>
2023-02-05 12:44:21 -0300

    Usando o place x=1px, e y=25px.

--- Criar-uma-janela-com-tkinter-Python.py
+++ Criar-uma-janela-com-tkinter-Python.py
@@ -3,6 +3,6 @@ from tkinter import ttk
 
 minha_aplicacao= Tk()
 
-texte_inicial = ttk.Label(text = "Olá Márcio José!!!")
+texte_inicial = ttk.Label(text = "Olá Márcio José!!!").place(x=1, y=25)
 
 minha_aplicacao.mainloop()
