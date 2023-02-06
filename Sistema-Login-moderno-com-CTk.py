95e566821dc857015d98fd3b672b43fd375dee29
Marcio Jose Alves <marciojosealves2@gmail.com>
2023-02-06 17:08:06 -0300

    Botão de cadastro e correção de cores.

--- Sistema-Login-moderno-com-CTk.py
+++ Sistema-Login-moderno-com-CTk.py
@@ -5,6 +5,7 @@ from tkinter import *
 
 janela = ctk.CTk()
 
+
 class Aplicacao():
 
     def __init__(self):
@@ -14,7 +15,6 @@ class Aplicacao():
         self.elementos_janela_basica()
         janela.mainloop()
 
-        
     def tema(self):
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("green")
@@ -26,30 +26,42 @@ class Aplicacao():
         janela.wm_resizable(width=False, height=False)
 
     def elementos_janela_basica(self):
-        #Parte de trabalho relativa a imagem da tela.
-        imagem_janela = PhotoImage( file="logo-mjamf.png")
-        label_imagem_janela = ctk.CTkLabel(master=janela, image=imagem_janela, text = None )#text = None ou '' para retirar o CTkLabel
+        # Parte de trabalho relativa a imagem da tela.
+        imagem_janela = PhotoImage(file="logo-mjamf.png")
+        # text = None ou '' para retirar o CTkLabel
+        label_imagem_janela = ctk.CTkLabel(
+            master=janela, image=imagem_janela, text=None).place(x=45, y=100)
+        label_titulo = ctk.CTkLabel(master=janela, text='Entre no Sistema de Login MJAMF', font=(
+            'Arvo', 18), text_color='green').place(x=23, y=10)
 
-        label_imagem_janela.place(x=45,y=100)
-        label_titulo=ctk.CTkLabel(master=janela, text='Entre no Sistema de Login MJAMF', font=('Arvo',18), text_color='green').place(x=23,y=10)
-
-        #Frame de inclusão de dados
-        login_frame = ctk.CTkFrame(master = janela, width = 350, height = 396)
+        # Frame de inclusão de dados
+        login_frame = ctk.CTkFrame(master=janela, width=350, height=396)
         login_frame.pack(side=RIGHT)
 
-        #Frame Widgets e elementos de inclusão de dados
-        nome_frame = ctk.CTkLabel(master=login_frame, text=('Sistema de Login'), font=('Arvo',20))#Veja https://github.com/TomSchimansky/CustomTkinter/wiki/CTkLabel
-        nome_frame.place(x=25,y=5)
+        # Frame Widgets e elementos de inclusão de dados
+        nome_frame = ctk.CTkLabel(master=login_frame, text=('Sistema de Login'), font=('Arvo', 20)).place(
+            x=25, y=5)  # Veja https://github.com/TomSchimansky/CustomTkinter/wiki/CTkLabel
 
-        nome_usuario = ctk.CTkEntry(master=login_frame, placeholder_text=('Nome do Usuário'), font=('Arvo',14), width=300).place(x=25,y=105)
-        label_nome_usuario = ctk.CTkLabel(master=login_frame, text='*O campo Nome do Usuário é obrigatório!', font=('Arvo',10), text_color='green').place(x=25,y=135)
+        nome_usuario = ctk.CTkEntry(master=login_frame, placeholder_text=(
+            'Nome do Usuário'), font=('Arvo', 14), width=300).place(x=25, y=105)
+        label_nome_usuario = ctk.CTkLabel(
+            master=login_frame, text='*O campo Nome do Usuário é obrigatório!', font=('Arvo', 10), text_color='green').place(x=25, y=135)
 
-        senha_usuario = ctk.CTkEntry(master=login_frame, placeholder_text=('Senha do Usuário'), font=('Arvo',14), width=300, show ='*').place(x=25,y=175)
-        label_senha_usuario = ctk.CTkLabel(master=login_frame, text='*O campo Senha do Usuário é obrigatório!', font=('Arvo',10), text_color='green').place(x=25,y=205)
+        senha_usuario = ctk.CTkEntry(master=login_frame, placeholder_text=(
+            'Senha do Usuário'), font=('Arvo', 14), width=300, show='*').place(x=25, y=175)
+        label_senha_usuario = ctk.CTkLabel(
+            master=login_frame, text='*O campo Senha do Usuário é obrigatório!', font=('Arvo', 10), text_color='green').place(x=25, y=205)
 
-        marcador_checagem = ctk.CTkCheckBox(master= login_frame, text='Lembrar depois.').place(x=25,y=235)
+        marcador_checagem = ctk.CTkCheckBox(
+            master=login_frame, text='Lembrar depois.').place(x=25, y=235)
 
-        botao_login = ctk.CTkButton(master =login_frame, text ='LOGIN', width=300).place(x=25,y=285)
+        botao_login = ctk.CTkButton(
+            master=login_frame, text='LOGIN', width=300).place(x=25, y=285)
+
+        label_resgistro = ctk.CTkLabel(
+            master=login_frame, text='Faça sau conta.'). place(x=25, y=325)
+        botao_registro = ctk.CTkButton(master=login_frame, text='Cadastro', width=190,
+                                       fg_color='#d75413', hover_color='#ff8000').place(x=135, y=325)
 
 
 Aplicacao()
