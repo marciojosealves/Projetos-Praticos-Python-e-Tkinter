ddb9868955babc133824e4b8159072c0e6fc6ccb
Marcio Jose Alves <marciojosealves2@gmail.com>
2023-02-10 13:36:47 -0300

    Inclusão de + regras de erro para login e registro

--- Sistema-Login-moderno-com-CTk.py
+++ Sistema-Login-moderno-com-CTk.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 from tkinter import *
 import sqlite3
 from tkinter import messagebox
+from string import ascii_letters # letras ASCII (todas as letras de "a" a "z", maiúsculas e minúsculas)
+
 
 
 class lado_servidor():
@@ -47,35 +49,64 @@ class lado_servidor():
 
         self.cursor.execute("""
 
-            INSERT INTO Usuarios (Nome_usuario, Email_usuario, Senha_usuario, Confirme_senha_usuario)
-            VALUES (?,?,?,?)""", (self.nome_usuario_entrada, self.email_usuario_entrada, self.senha_usuario_entrada, self.confirme_senha_entrada))
+            #INSERT INTO Usuarios (Nome_usuario, Email_usuario, Senha_usuario, Confirme_senha_usuario)
+            #VALUES (?,?,?,?)""", (self.nome_usuario_entrada, self.email_usuario_entrada, self.senha_usuario_entrada, self.confirme_senha_entrada))
 
         try:
             if(self.nome_usuario_entrada == "" or self.email_usuario_entrada == "" or self.senha_usuario_entrada == "" or self.confirme_senha_entrada == ""):
                 messagebox.showerror(title= "Sistema de Login",message="Todos os campos devem ser preenchidos!")
-            elif(len(self.nome_usuario_entrada)<4):
-                messagebox.showwarning(title="Sistema de Login", message=" O nome deve ter pelo menos quatro caracteres.")
+            elif all(caracter in (ascii_letters + 'áéíóú') for caracter in self.nome_usuario_entrada):
+                messagebox.showwarning(title="Sistema de Login", message="Por favor digite um nome válido com pelo menos,\nnome e último nome.\n(somente letras e espaços)") # (ascii_letters + 'áéíóú') acrescenta o espaço e algumas letras acentuadas, adicione tudo que precisar aqui
             elif(len(self.senha_usuario_entrada)<4):
                 messagebox.showwarning(title="Sistema de Login", message=" O senha deve ter pelo menos quatro caracteres.")
             elif(self.senha_usuario_entrada != self.confirme_senha_entrada):
                 messagebox.showerror(title="Sistema de Login", message="Senhas não são iguais!")
             else:
                 self.faca_conexao.commit()
-                messagebox.showinfo(title="sistema de Login", message=f"Dados cadastrados com sucesso!\n{self.nome_usuario_entrada} seja bem-vindo!")
+                messagebox.showinfo(title="sistema de Login", message=f"Dados cadastrados com sucesso!\n{self.nome_usuario_entrada} seja BEM-VINDO!")
                 print("Dados cadastrados com sucesso!")
-                self.desconexao_banco_dados() 
+                #print(self.nome_usuario_entrada)
+                self.desconexao_banco_dados()
+                self.limpa_cadastro() 
             
         except:
             messagebox.showerror(title="Sistema de Login", message="Erro no processamento do seu pedido!\n Tente novamente mais tarde.")
+            self.desconexao_banco_dados()
         
-            ''' from string import ascii_letters # letras ASCII (todas as letras de "a" a "z", maiúsculas e minúsculas)
+            ''' '''
 
-            elif all(c in (ascii_letters + 'áéíóú') for c in self.nome_usuario_entrada):
-                messagebox.showwarning(title="Sistema de Login", message="Por favor digite um nome válido (somente letras e espaços).") # (ascii_letters + 'áéíóú') acrescenta o espaço e algumas letras acentuadas, adicione tudo que precisar aqui'''
+    def verificar_login(self):
 
-        '''self.faca_conexao.commit()
-        print("Dados cadastrados com sucesso!")
-        self.desconexao_banco_dados()'''
+        self.nome_usuario_login_verifica = self.nome_usuario_login.get()
+        self.senha_usuario_login_verifica = self.senha_usuario_login.get()
+
+        self.conexao_banco_dados()
+
+        self.cursor.execute("""
+
+            SELECT * FROM Usuarios WHERE (Nome_usuario = ? AND Senha_usuario = ?)""", (self.nome_usuario_login_verifica, self.senha_usuario_login_verifica))
+         
+        self.verifica_dados = self.cursor.fetchone() # Percorre a tabela Usuarios é indicado .fetchone(),mas pode usar .fetchall()
+
+        try:
+            if(self.nome_usuario_login_verifica == "" or self.senha_usuario_login_verifica == ""):
+                messagebox.showwarning(title = "Sistema de Login", message = "Os campos devem ser todos preenchidos.")
+            elif(self.nome_usuario_login_verifica in self.verifica_dados and self.senha_usuario_login_verifica in self.verifica_dados):
+                 messagebox.showinfo( title = "Sistema de Login", message = f"BEM-VINDO {self.nome_usuario_login_verifica}\n Login realizado com sucesso.")
+                 self.desconexao_banco_dados()
+                 self.limpa_login()# Se hovesse um painel, seria encaminhado para ele.
+            
+
+        except:
+            messagebox.showerror(title="Sistema de Login", message="Usuário não encontrado no sistema.\nVerifique seus dados ou faça seu cadastro.")
+            self.desconexao_banco_dados() 
+            #Aqui não limpamos os dados para usuário verificar se digitou corretamente.        
+
+
+         #print(self.nome_usuario_login_verifica,self.senha_usuario_login_verifica)
+         
+
+ 
 
 
 class aplicativo(ctk.CTk, lado_servidor):
@@ -145,7 +176,7 @@ class aplicativo(ctk.CTk, lado_servidor):
             row=5, column=0, padx=10, pady=1)
 
         self.botao_login = ctk.CTkButton(
-            self.login_frame, text='LOGIN', width=300, font=('Arvo', 16), fg_color='#d75413', hover_color='#ff8000', corner_radius=15)
+            self.login_frame, text='LOGIN', width=300, font=('Arvo', 16), fg_color='#d75413', hover_color='#ff8000', corner_radius=15, command=self.verificar_login)
         self.botao_login.grid(row=6, column=0, padx=15, pady=15)
 
         self.label_resgistro = ctk.CTkLabel(
